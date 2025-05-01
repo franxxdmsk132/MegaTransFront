9e645aaca60e9d6198655d71fbb29b0753543820
franxxdsmk132 <tapiafranklin666@gmail.com>
2025-05-01 15:21:18 -0500

    Enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+
   apiUrl: 'https://665e-45-236-151-3.ngrok-free.app',
   wsUrl: 'wss://665e-45-236-151-3.ngrok-free.app/ws',
   chatbot: 'https://d077-45-236-151-3.ngrok-free.app'
