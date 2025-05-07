f939682db94e8ca00c53807bd416625e1450049a
franxxdmsk <tapiafranklin666@gmail.com>
2025-05-07 15:10:19 -0500

    Whatsapp

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   apiUrl: 'https://63b6-45-236-151-3.ngrok-free.app',
   wsUrl: 'wss://63b6-45-236-151-3.ngrok-free.app/ws',
-  // chatbot: 'https://7d90-45-236-151-3.ngrok-free.app'
-  chatbot: 'http://localhost:3000'
+  chatbot: 'https://7d90-45-236-151-3.ngrok-free.app'
+  // chatbot: 'http://localhost:3000'
 
 
   //apiUrl: 'http://104.196.61.204:8080',
